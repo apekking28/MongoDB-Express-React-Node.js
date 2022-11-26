fd0ad38e9691332dd97e304241f2a990467cc8d7
Ilham firmansyah <apekking28@gmail.com>
2022-11-27 04:24:07 +0700

    skuyy

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+import EditUser from "./components/EditUser";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<UserList />} />
+            <Route path="add" element={<AddUser />} />
+            <Route path="edit/:id" element={<EditUser />} />
+          </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
